70a287e3df5d73a2240014eb6186bbb69b2b4818
Mustaqeem Khowaja <mustaqeem.khowaja@gmail.com>
2013-04-29 14:44:34 -0300

    Create Table.java

---
+++ Table.java
@@ -0,0 +1,121 @@
+public class Table {
+  private double height;
+  private double length;
+	private double width;
+	private int legs;
+	private double weight;
+	private String shape;
+	private String colour;
+
+	public double getHeight(){
+		return height;
+	}
+	public double getLength(){
+		return length;
+	}
+	public double getWidth(){
+		return width;
+	}
+	public int getLegs(){
+		return legs;
+	}
+	public double getWeight(){
+		return weight;
+	}
+	public String getShape(){
+		return shape;
+	}
+	public String getColour(){
+		return colour;
+	}
+	public void putHeight (double x){
+		if (x>0 && x<=50){
+			height = x;
+		}else{
+			System.out.println("Error");
+		}
+	}
+	public void putLength (double x){
+		if (x>0 && x<=100){
+			length = x;
+		}else{
+			System.out.println("Error");
+		}
+	}
+	public void putWidth (double x){
+		if (x>0 && x<=100){
+			width = x;
+		}else{
+			System.out.println("Error");
+		}
+	}
+	public void putLegs (int x){
+		if (x>0 && x<=4){
+			legs = x;
+		}else{
+			System.out.println("Error");
+		}
+	}
+	public void putWeight (double x){
+		weight = x;
+	}
+	public void putShape (String x){
+		shape = x;
+	}
+	public void putColour (String x){
+		colour = x;
+	}
+	public double Calculatearea(double length, double weight) {
+		double area = length*width;
+		return area;
+	}
+	public double Calculateperimeter (double length, double width){
+		double perimeter = 2*(length+width);
+		return perimeter;
+	}
+	public double Calculatesurfacearea(double length, double width, double height){
+		double surfacearea = 2*(length*width) + 2*(length*height) + 2*(weight*height);
+		return surfacearea;
+	}
+	public double Calculateweightperleg (int legs, double weight){
+		double weightperleg = weight/legs;
+		return weightperleg;
+	}
+	public void Displaydimensions (){
+		double [] d = {this.length,this.height,this.width};
+		double temp;
+		boolean swap;
+		for (int i = 0; i<d.length-1;i++){
+			swap = false;
+			do{
+				if (d[i]>d[i+1]){
+					temp = d[i];
+					d[i] = d[i+1];
+					d[i+1]=temp;
+				}
+			}while (swap == true);
+		}
+		System.out.println(d[0]+" x "+d[1]+" x "+d[2]);
+	}
+	public void Displaytable(){
+		System.out.println("Height = " + height + "\nLength = " + length + "\nWidth = " + width + "\nLegs = " + legs + "\nWeight = " + weight + "\nShape = " + shape + "\nColour = " + colour);
+	}
+	public void copytable (Table x){
+		height = x.getHeight();
+		length = x.getLength();
+		width = x.getWidth();
+		legs = x.getLegs();
+		weight = x.getWeight();
+		shape = x.getShape();
+		colour = x.getColour();
+	}
+	public void copylargertable (Table x, Table y){
+		Table larger = new Table ();
+		if (x.Calculatesurfacearea(x.getLength(), x.getWidth(), x.getHeight())>=y.Calculatesurfacearea(y.getLength(), y.getWidth(), y.getHeight())){
+			larger.putHeight(x.getHeight());
+			larger.putLength(x.getLength());
+		}else{
+			larger = y;
+		}
+	}
+}
