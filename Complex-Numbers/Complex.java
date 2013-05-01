d350c76db499dd90e33f8068214ef036126f80fa
Mustaqeem Khowaja <mustaqeem.khowaja@gmail.com>
2013-05-01 15:03:22 -0300

    Update and rename complex.java to Complex-Numbers/Complex.java

--- Complex-Numbers/Complex.java
+++ Complex-Numbers/Complex.java
@@ -1,6 +1,7 @@
-public class Complex2 {
-  double re;
+public class Complex {
+  	double re;
 	double im;
+	
 	public double modulus (){
 		return Math.sqrt(Math.pow(this.re,2) + Math.pow(this.im,2));
 	}
@@ -8,14 +9,14 @@ public class Complex2 {
 		this.re *=x;
 		this.im*=x;
 	}
-	public Complex2 sum (Complex2 other){
-		Complex2 result = new Complex2();
+	public Complex sum (Complex other){
+		Complex result = new Complex();
 		result.re = this.re+other.re;
 		result.im = this.im + other.im;
 		return result;
 	}
-	public Complex2 times (Complex2 other){
-		Complex2 result = new Complex2();
+	public Complex times (Complex other){
+		Complex result = new Complex();
 		result.re = this.re*other.re + this.re*this.im*-1;
 		result.im = this.re * other.im + this.im * other.re;
 		return result;
