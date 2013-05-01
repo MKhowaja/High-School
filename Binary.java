ec8365e8f29029d0baa49785d57525be97602106
Mustaqeem Khowaja <mustaqeem.khowaja@gmail.com>
2013-05-01 01:53:45 -0300

    Create Binary.java
    Converts a String of binary to text

---
+++ Binary.java
@@ -0,0 +1,28 @@
+import java.io.*;
+public class Binary {
+
+public static void main(String[] args) throws IOException {
+String a = "000101010100010101000101010101010010101000001010";
+int x, z=0;
+String temp="";
+int total=0;
+StringBuilder message=new StringBuilder();
+for (int i = 0; i < a.length()/8;i++){
+temp = a.substring(i*8,i*8+8);
+for (int j = 7; j>=0;j--){
+
+x = (int) temp.charAt(j)-'0';
+if (x==1){
+total += Math.pow(2, z);
+}
+z++;
+}
+message.append((char)total);
+total = 0;
+z=0;
+}
+System.out.println(message);
+
+}
+
+}
